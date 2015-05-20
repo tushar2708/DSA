380aae70441afc566808e0cb5f78d657b5e3275b
tushar2708 <tdwivedi2708@gmail.com>
2015-05-20 18:08:41 +0530

    Few new additions

--- DSA/Linked_List/linked_list.cpp
+++ DSA/Linked_List/linked_list.cpp
@@ -15,7 +15,7 @@ class Node{
 	int data;
 	Node * next;
 
-public:
+	public:
 	Node(){};
 	void SetData(int aData){data = aData;};
 	void SetNext(Node * aNext){next = aNext;};
@@ -31,8 +31,24 @@ class List{
 	public:
 	List() {First = NULL;};
 	void Print();
+	void Prepend(int data);
 	void Append(int data);
+	void Sorted_Insert(int data);
+
 	void Delete(int data);
+	int Length();
+	int GetNthNode(int n);
+	void Delete_using_ptr_wrapper(){Delete_using_ptr(((First->GetNext())->GetNext())->GetNext());};
+	//void Delete_using_ptr_wrapper(){Delete_using_ptr(First->GetNext());};
+	void Delete_using_ptr(Node * node);
+	int GetMiddleElem();
+	void Delete_list();
+	void Reverse();
+	void Create_loop();
+	int Detect_loop();
+	void Delete_Duplicates();
+
+
 
 };
 
@@ -49,6 +65,8 @@ class Stack{
 
 };
 
+
+
 void List::Print(){
 
 	Node * temp = First;
@@ -59,7 +77,7 @@ void List::Print(){
 			printf("%d-->", temp->GetData());
 			temp = temp->GetNext();
 		}
-	printf("NULL\n");
+		printf("NULL\n");
 	}
 
 }
@@ -76,13 +94,45 @@ void List::Append(int data){
 		First = new_node;
 	} else {
 
-	while(temp->GetNext() != NULL){
-		temp = temp->GetNext();
-	}
-	temp->SetNext(new_node);
+		while(temp->GetNext() != NULL){
+			temp = temp->GetNext();
+		}
+		temp->SetNext(new_node);
 
 
 	}
+}
+
+void List::Prepend(int data){
+
+	Node* new_node = new Node();
+
+	new_node->SetData(data);
+	new_node->SetNext(First);
+
+	First = new_node;
+}
+
+void List::Sorted_Insert(int data)
+{
+	Node * current = First;
+	Node * new_node = new Node();
+
+	new_node->SetData(data);
+
+	if(First == NULL || First->GetData() >= data){
+		new_node->SetNext(First);
+		First = new_node;
+	} else {
+		while(current->GetNext() != NULL && (current->GetNext())->GetData() <= data)
+			current = current->GetNext();
+
+		new_node->SetNext(current->GetNext());
+		current->SetNext(new_node);
+	}
+
+
+
 }
 
 void List::Delete(int data){
@@ -99,9 +149,9 @@ void List::Delete(int data){
 					delete current;
 					return;
 				} else {
-				prev->SetNext(current->GetNext());
-				delete current;
-				return;
+					prev->SetNext(current->GetNext());
+					delete current;
+					return;
 				}
 
 			}
@@ -113,29 +163,170 @@ void List::Delete(int data){
 	}
 }
 
+void List::Delete_using_ptr(Node * node){
 
+	Node * prev = First;
+	Node * current = First;
+	if(First == NULL){
+		return;
+	} else {
+		while(current != NULL){
+			if(current == node ){
+				if(current == First){
+					First = First->GetNext();
+					delete current;
+					return;
+				} else {
+					prev->SetNext(current->GetNext());
+					delete current;
+					return;
+				}
 
-/*
-int main() {
-	// your code goes here
-	int i, x, N = 0;
-	List list;
+			}
+			prev = current;
+			current = current->GetNext();
 
-	scanf("%d\n", &N);
-	for(i=0; i<N; i++){
-		scanf("%d ", &x);
-		list.Append(x);
+		}
+
 	}
-
-	list.Print();
-	scanf("%d\n", &N);
-	for(i=0; i<N; i++){
-		scanf("%d ", &x);
-		list.Delete(x);
-	}
-
-	list.Print();
-
-	return 0;
 }
-*/
+
+int List::Length()
+{
+
+	int count = 0;
+	Node * temp = First;
+
+	while (temp != NULL){
+
+		count++;
+		temp = temp->GetNext();
+
+	}
+
+	return count;
+
+}
+
+int List::GetNthNode(int n)
+{
+	int i = 0;
+	Node * temp = First;
+	for (i=0; i < n; i++){
+		temp = temp->GetNext();
+		if (temp == NULL)
+			return -1;
+	}
+	return temp->GetData();
+}
+
+int List::GetMiddleElem(){
+
+	Node * slow_ptr = First,  * fast_ptr = First;
+
+	while (fast_ptr != NULL && fast_ptr->GetNext() != NULL){
+
+		slow_ptr = slow_ptr->GetNext();
+		fast_ptr = (fast_ptr->GetNext())->GetNext();
+
+	}
+
+	return slow_ptr->GetData();
+
+}
+
+void List::Delete_list()
+{
+
+	Node * current = First;
+	Node * next;
+	while (current != NULL) {
+
+		next = current->GetNext();
+		delete current;
+		current = next;
+	}
+
+	First = NULL;
+
+}
+
+
+void List::Reverse()
+{
+
+	Node * prev = NULL, * next = NULL;
+	Node * current = First;
+
+	while (current != NULL){
+
+		next = current->GetNext();
+		current->SetNext(prev);
+
+		prev = current;
+		current = next;
+	}
+
+	First = prev;
+
+}
+
+
+void List::Create_loop()
+{
+	Node * current = First;
+
+	while (current->GetNext() != NULL)
+		current = current->GetNext();
+
+
+	current->SetNext(First->GetNext());
+
+}
+
+
+int List::Detect_loop()
+{
+	Node * slow_ptr = First,  * fast_ptr = First;
+
+	while (slow_ptr != NULL && fast_ptr != NULL && fast_ptr->GetNext() != NULL) {
+
+		slow_ptr = slow_ptr->GetNext();
+		fast_ptr = (fast_ptr->GetNext())->GetNext();
+
+		if (slow_ptr == fast_ptr) {
+
+			printf("\nFound Loop\n");
+			return 1;
+
+		}
+
+	}
+
+	printf("\nDidn't Find Loop\n");
+	return 0;
+
+}
+
+
+void List::Delete_Duplicates()
+{
+
+	Node * current, * next, *next_next;
+
+	current = First;
+	while(current->GetNext() != NULL) {
+
+		next = current->GetNext();
+
+		if (current->GetData() == next->GetData()) {
+			next_next = next->GetNext();
+			delete next;
+			current->SetNext(next_next);	
+		} else {
+			current = next;
+		}
+	}
+
+}
+
--- DSA/Linked_List/linked_list_test.cpp
+++ DSA/Linked_List/linked_list_test.cpp
@@ -10,25 +10,84 @@
 
 #include "linked_list.cpp"
 
-int main() {
+int main() 
+{
 	// your code goes here
 	int i, x, N = 0;
-	List list;
+	List list ;
 
-	scanf("%d\n", &N);
-	for(i=0; i<N; i++){
-		scanf("%d ", &x);
-		list.Append(x);
-	}
+	printf ("Length = %d\n", list.Length());
+
+	list.Append(3);
+	list.Append(4);
+	list.Append(5);
+
+	printf ("Length = %d\n", list.Length());
+	list.Print();
+
+	list.Prepend(2);
+	//list.Prepend(1);
+	
+	printf ("Length = %d\n", list.Length());
+	list.Print();
+
+	list.Append(9);
+
+	list.Sorted_Insert(12);
+	list.Sorted_Insert(1);
+	list.Sorted_Insert(0);
+	list.Sorted_Insert(6);
+	list.Sorted_Insert(0);
+	list.Sorted_Insert(3);
+	list.Sorted_Insert(11);
+	list.Sorted_Insert(12);
+	list.Sorted_Insert(7);
+	list.Sorted_Insert(7);
+	list.Sorted_Insert(8);
+	list.Sorted_Insert(10);
+	list.Sorted_Insert(4);
+	list.Print();
+
+	printf("Middle Element : %d\n", list.GetMiddleElem());
+
+	//list.Reverse();
+
+	//printf("Reversed : ");	
+	//list.Print();
+
+	list.Delete(3);
+
+	printf ("Length = %d\n", list.Length());
+	list.Print();
+
+	printf("0 : %d\n", list.GetNthNode(0));
+
+	printf("3 : %d\n", list.GetNthNode(3));
+
+	printf("10 : %d\n", list.GetNthNode(10));
+
+	//list.Delete_using_ptr_wrapper();
 
 	list.Print();
-	scanf("%d\n", &N);
-	for(i=0; i<N; i++){
-		scanf("%d ", &x);
-		list.Delete(x);
-	}
 
+	//list.Create_loop();
+	int isLoop = list.Detect_loop();
+
+	list.Delete_Duplicates();
+        printf ("Length = %d\n", list.Length());
+        list.Print();
+
+
+
+
+
+	if(isLoop == 0) {
+	list.Delete_list();
+
+	printf ("Length = %d\n", list.Length());
 	list.Print();
+
+	}
 
 	return 0;
 }
